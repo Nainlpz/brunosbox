94e63a4a09073904c19904a62d9aa7af7a76baa7
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-17 14:35:06 +0100

    feat(RoundFactory): Crear clase RoundFactory

---
+++ src/main/java/edu/estatuas/brunosbox/RoundFactory.java
@@ -0,0 +1,22 @@
+package edu.estatuas.brunosbox;
+
+public class RoundFactory {
+
+    public static Round getRound(String roundScore) {
+
+        if (roundScore == null) {
+            return null;
+        }
+        if (roundScore.equalsIgnoreCase("10 - 9")
+                || roundScore.equalsIgnoreCase("9 - 10")) {
+            return new RegularRound(roundScore);
+        }
+        else if (roundScore.equalsIgnoreCase("10 - 8")
+                || roundScore.equalsIgnoreCase("8 - 10")) {
+            return new KnockdownRound(roundScore);
+        }
+        return null;
+    }
+}
+
+
