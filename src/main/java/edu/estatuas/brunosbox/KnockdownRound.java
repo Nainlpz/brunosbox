82da3a0e388fcd6b8a9b9127867552284e93b7a9
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-17 14:23:22 +0100

    feat(KnockdownRound): Crear clase KnockdownRound

---
+++ src/main/java/edu/estatuas/brunosbox/KnockdownRound.java
@@ -0,0 +1,39 @@
+package edu.estatuas.brunosbox;
+
+public class KnockdownRound implements Round {
+
+    private final String roundScore;
+    private byte redBoxerScore;
+    private byte blueBoxerScore;
+
+    KnockdownRound(String roundScore) {
+        this.roundScore = roundScore.replaceAll("\\s", "");
+        this.boxerRoundScore();
+    }
+
+    String getRoundScore() {
+        return this.roundScore;
+    }
+
+    void boxerRoundScore() {
+        int dashIndex = this.roundScore.indexOf('-');
+        this.redBoxerScore = Byte.parseByte(this.roundScore.substring(0, dashIndex));
+        this.blueBoxerScore = Byte.parseByte(this.roundScore.substring(dashIndex + 1));
+    }
+
+    @Override
+    public byte getRedBoxerScore() {
+        return this.redBoxerScore;
+    }
+
+    @Override
+    public byte getBlueBoxerScore() {
+        return this.blueBoxerScore;
+    }
+
+    @Override
+    public String toString() {
+        return this.getRedBoxerScore() + " - " + this.getBlueBoxerScore();
+    }
+
+}
